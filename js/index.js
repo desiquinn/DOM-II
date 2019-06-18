a12d749d71ffaddc0fd5ff6dc81d11e62d557482
Desiree Morris <desiquinncompany@gmail.com>
2019-06-18 16:02:44 -0400

    added the mouseover event to the first h2 header

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 // Your code goes here
 
-const headerColor = document.querySelectorAll('h2');
+const headerColor = document.querySelector('h2');
 
-headerColor.addEventListener("click", function (title) {
-    title.target.style.color = "blue";
+headerColor.addEventListener("mouseover", function (event) {
+    event.target.style.color = "#17A2B8";
 });
 
 // console.log(headerColor)
