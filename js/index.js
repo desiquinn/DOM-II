d075015e4f16381574fcda93b58ef93b2612f0b8
Desiree Morris <desiquinncompany@gmail.com>
2019-06-18 15:47:43 -0400

    having a difficult time getting mouseover to work

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+const headerColor = document.querySelectorAll('h2');
+
+headerColor.addEventListener("click", function (title) {
+    title.target.style.color = "blue";
+});
+
+// console.log(headerColor)
