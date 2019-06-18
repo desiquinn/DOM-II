61b30d92cbf36ef8ab35e7f5b2d50f73ff211f6c
Desiree Morris <desiquinncompany@gmail.com>
2019-06-18 18:15:29 -0400

    added a click and dblclick event

--- js/index.js
+++ js/index.js
@@ -1,9 +1,44 @@
 // Your code goes here
 
-const headerColor = document.querySelector('h2');
+const headerColor = document.querySelectorAll('h2');
 
-headerColor.addEventListener("mouseover", function (event) {
-    event.target.style.color = "#17A2B8";
+headerColor.forEach((item) => {
+    item.addEventListener("mouseover", event => {
+        event.target.style.color = "#17A2B8";
+    });
+
+    item.addEventListener("mouseout", event => {
+        event.target.style.color = "black";
+    });
 });
 
-// console.log(headerColor)
+const highlight = document.querySelector('.text-content');
+
+highlight.addEventListener("dblclick", event => {
+    event.target.style.backgroundColor = "yellow"
+    });
+
+const buttonClick = document.querySelectorAll('.btn');
+
+buttonClick.forEach( (item) => {
+    item.addEventListener("click", event => {
+        event.target.style.backgroundColor = "pink";
+    });
+});
+
+const headerDrag = document.querySelectorAll('h4');
+
+headerDrag.forEach( (item) => {
+    item.addEventListener("dragstart", event => {
+        event.target.style.border = "1px solid red";
+    });
+
+    item.addEventListener("drag", event => {
+        event.target.style.border = "1px dotted black";
+    });
+
+    item.addEventListener("dragend", event => {
+        event.target.style.border = "1px dashed purple";
+    });
+});
+
